82a686f4958bdc7832476e45237c30a51dbdbeaf
aokm <m.aoki1987@gmail.com>
2022-01-26 22:31:54 +0900

    create posts controller

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,18 @@ module RailsApp
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.helper false
+      g.test_framework false
+      # g.test_framework :rspec,
+        # fixtures: false,
+        # view_specs: false,
+        # helper_specs: false,
+        # routing_specs: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
